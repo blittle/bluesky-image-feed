c722438e33330a48cbb8b751f7f4860606f170be
Bret Little <bret.little@gmail.com>
2025-10-05 17:10:17 -0400

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
+  build: {
+    lib: {
+      entry: "src/bluesky-image-feed.js",
+      formats: ["es"],
+      fileName: () => "bluesky-image-feed.js",
+    },
+    rollupOptions: {
+      output: {
+        dir: "dist",
+      },
+    },
+  },
 });
